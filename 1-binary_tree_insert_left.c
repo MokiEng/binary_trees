603f8e92c9cd1bce73a11dafb5e133f3bfcfa3ec
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-05-30 07:33:12 -0700

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
  * binary_tree_insert_left - a function which inserts
- * a node as the left-child of another node.
+ *	a node as the left-child of another node.
  * @parent: A pointer to the node to insert the left child in.
  * @value: The value to store the new node.
  *
@@ -11,25 +10,26 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
 	if (parent == NULL)
 	{
 		return (NULL); /* Cannot insert left child if parent is NULL */
 	}
 	/* Create the new node */
-	binary_tree_t *new_node = binary_tree_node(parent, value);
+	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
-	{
+
 		return (NULL); /* Unable to create a new node */
-	}
-	if (parent->left != NULL)
-	{
-		new_node->left = parent->left;
-		/* Set the parent of the current left child to the new node */
-		parent->left->parent = new_node;
-		/* Set the current left child as the left child of the new node */
-	}
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = NULL;
+	new_node->left = parent->left;
 	parent->left = new_node;
-	/* Set the new node as the left child of the parent */
+
+	if (new_node->left)
+		new_node->left->parent = new_node;
 	return (new_node);
 }
