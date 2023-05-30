b710f528eb5eab74c9498ca1793126b3d8ce5bc0
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-05-30 07:57:38 -0700

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,14 +14,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_height = 0;
 
 	if (tree == NULL)
-		return (0);/* If tree is NULL, return height as 0 */
+	{
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		/**
+		*  Return: the maximum height between the left and
+		*	right subtrees, plus 1 for the current node
+		*/
+		return (left_heigh > right_height ? left_heigh : right_height);
+	}
 
-	/**
-	 *  Return: the maximum height between the left and
-	 *	right subtrees, plus 1 for the current node
-	 */
-	return (left_heigh > right_height ? left_heigh : right_height);
+	return (0);/* If tree is NULL, return height as 0 */
 }
