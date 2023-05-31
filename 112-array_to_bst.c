0405bc84d625795e281637a42fa3dc1f64052839
MokiEng <meconen20@gmail.com>
2023-05-31 12:08:04 -0700

    array

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -10,35 +10,36 @@
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
-  bst_t *root = NULL;
- 
-  for (size_t i = 0; i < size; i++)
-  {
-    int value = array[i];
-   
-    /* Check if the value already exists in the BST */
-    bst_t *current = root;
-    int found = 0;
-    while (current != NULL)
-    {
-      if (value == current->data)
-      {
-        found = 1;
-        break;
-      }
-      else if (value < current->data)
-      {
-        current = current->left;
-      }
-      else
-      {
-        current = current->right;
-      }
-    }
-    if (!found)
-    {
-      root = insert_bst_node(root, value);
-    }
-  }
-  return (root);
+	bst_t *root = NULL;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		int value = array[i];
+
+		/* Check if the value already exists in the BST */
+		bst_t *current = root;
+		int found = 0;
+
+		while (current != NULL)
+		{
+			if (value == current->data)
+			{
+				found = 1;
+				break;
+			}
+			else if (value < current->data)
+			{
+				current = current->left
+			}
+			else
+			{
+				current = current->right;
+			}
+		}
+		if (!found)
+		{
+			root = insert_bst_node(root, value);
+		}
+	}
+	return (root)
 }
