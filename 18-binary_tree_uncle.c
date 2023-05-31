e75b18429a3328afac59249410323d1cdcb9cbc5
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-05-31 02:17:21 -0700

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,20 +8,26 @@
  * Return: NULL if node is NULL
  *	or node has no uncle.
  */
-binary_tree_t *binary_tree_uncle(binary_tree_t *node) {
-    if (node == NULL || node->parent == NULL || node->parent->parent == NULL) {
-        return NULL; // If node, parent, or grandparent is NULL, return NULL
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+    {
+        return (NULL);/* If node, parent, or grandparent is NULL, return NULL */
     }
 
     binary_tree_t *parent = node->parent;
     binary_tree_t *grandparent = parent->parent;
-
-    if (grandparent->left == parent) {
-        return grandparent->right; // Return the right child of the grandparent if parent is the left child
-    } else if (grandparent->right == parent) {
-        return grandparent->left; // Return the left child of the grandparent if parent is the right child
-    } else {
-        return NULL; // Return NULL if node has no uncle
+ 
+    if (grandparent->left == parent)
+    {
+        return (grandparent->right);
+    }
+    else if (grandparent->right == parent)
+    {
+        return (grandparent->left);
+    }
+    else
+    {
+        return (NULL);/* Return NULL if node has no uncle */
     }
 }
-
