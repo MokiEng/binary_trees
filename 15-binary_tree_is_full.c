2a814104950cb09010925563899d531ce9e01498
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-05-31 01:03:43 -0700

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,8 +1,5 @@
 #include "binary_trees.h"
 
-int binary_tree_is_full(const binary_tree_t *tree);
-int is_full_recursive(const binary_tree_t *tree);
-
 /**
  * binary_tree_is_full - a function that checks if a binary tree is full.
  *
@@ -24,19 +21,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);/* Tree with no nodes is considered full */
 	}
 
-	/* Check if the tree violates the conditions of a full binary tree */
-
-	if ((tree->left == NULL && tree->right != NULL)
-			|| (tree->left != NULL && tree->right == NULL))
-	{
-		return (0);/* Tree has a single child, violating the condition */
-	}
-
-	/* Recursively check if both subtrees are full */
-
-	int left_full = binary_tree_is_full(tree->left);
-	int right_full = binary_tree_is_full(tree->right);
-
-	/* Return 1 only if both subtrees are full */
-	return (left_full && right_full);
+	if (tree->right && tree->left)
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+	return (0);
 }
