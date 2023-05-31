dd73e4203a29482c98d57578201670877526434e
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-05-31 02:28:57 -0700

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -16,16 +16,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);/* If node or parent is NULL, return NULL */
 	}
 
-	binary_tree_t *parent = node->parent;
-
-	if (parent->left == node)
-	{
-		return (parent->right);
-	}
-	else if (parent->right == node)
-	{
-		return (parent->left);
-	}
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
 	else
 	{
 		return (NULL);
