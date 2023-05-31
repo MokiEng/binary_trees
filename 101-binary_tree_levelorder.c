ad5d9a58911cb339eda23986b45a6d317454bf9c
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-05-31 07:04:47 -0700

    Update 101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -37,3 +37,32 @@ void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level)
 		btlo_helper(tree->right, func, level - 1);
 	}
 }
+
+/**
+ * binary_tree_height - a function that measures the height of
+ *	a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: 0 if tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+	{
+		return (0);/* If tree is NULL, return height as 0 */
+	}
+	{
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+		/**
+		*  Return: the maximum height between the left and
+		*	right subtrees, plus 1 for the current node
+		*/
+		return (left_height > right_height ? left_height : right_height);
+	}
+}
