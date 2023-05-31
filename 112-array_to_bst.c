968ad6e75d1a86dd1ec29a325a09393f879e3969
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-05-31 11:33:42 -0700

    Create 112-array_to_bst.c

---
+++ 112-array_to_bst.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - a function that builds a Binary Search Tree from an array
+ *
+ * @array: a pointer to the first element of the array to be converted.
+ * @size: the number of element in the array
+ *
+ * Return: a pointer to the root node of the created BST, or NULL on failure.
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+  bst_t *root = NULL;
+ 
+  for (size_t i = 0; i < size; i++)
+  {
+    int value = array[i];
+   
+    /* Check if the value already exists in the BST */
+    bst_t *current = root;
+    int found = 0;
+    while (current != NULL)
+    {
+      if (value == current->data)
+      {
+        found = 1;
+        break;
+      }
+      else if (value < current->data)
+      {
+        current = current->left;
+      }
+      else
+      {
+        current = current->right;
+      }
+    }
+    if (!found)
+    {
+      root = insert_bst_node(root, value);
+    }
+  }
+  return (root);
+}
