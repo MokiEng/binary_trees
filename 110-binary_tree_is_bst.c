dd299853ed858889883fdb23e09f57dfb7174cfb
MokiEng <meconen20@gmail.com>
2023-06-01 02:57:22 -0700

    BST

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -6,25 +6,27 @@
  *
  * Return: 1 if tree is a valid BST, 0 otherwise.
  */
-int binary_tree_is_bst(const binary_tree_t* tree)
+int binary_tree_is_bst(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
-  return (is_bst_util(tree, INT_MIN, INT_MAX));
+	if (tree == NULL)
+		return (0);
+	return (is_bst_util(tree, INT_MIN, INT_MAX));
 }
-
 /**
-* is_bst_util -function is a recursive helper function that takes a binary
-* tree node along with a minimum value and a maximum value.
-* @node: a binary tree node.
-* @min_val: the minimum value.
-* @max_val: the maximum value.
-*/
-bool is_bst_util(const binary_tree_t* node, int min_val, int max_val)
+ * is_bst_util -function is a recursive helper function that takes a binar
+ *		tree node along with a minimum value and a maximum value.
+ * @node: a binary tree node.
+ * @min_val: the minimum value.
+ * @max_val: the maximum value.
+ *
+ * Return: the rotated subtree at the given node.
+ */
+bool is_bst_util(const binary_tree_t *node, int min_val, int max_val)
 {
-  if (node == NULL)
-    return (true);
-  if (node->val <= min_val || node->val >= max_val)
-    return (false);
-  return (is_bst_util(node->left, min_val, node->val) && is_bst_util(node->right, node->val, max_val));
+	if (node == NULL)
+		return (true);
+	if (node->val <= min_val || node->val >= max_val)
+		return (false);
+	return (is_bst_util(node->left, min_val, node->val) &&
+			is_bst_util(node->right, node->val, max_val));
 }
